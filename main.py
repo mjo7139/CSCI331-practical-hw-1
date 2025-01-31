c45fa8711367c23178899bd12258579a66c044a3
Matthew O'Neil <mjo7139@rit.edu>
2025-01-31 10:41:10 -0500

    program works
    Program works in the sense that it can find its way from start to target. However, it does not print the path and I don't believe it is working optimally. Although I will be able to see if it is optimal when I get it printing the path.

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ import sys
 
 def isEdge(str1, str2):
     # function to check if two strings are edges in the problem. I.e they 
-    # differ by one letter and are the same length
-    if len(str1) != len(str2):
-        return False
+    # differ by one letter
     count = 0
     for i in range(len(str1)):
         if str1[i] != str2[i]:
@@ -22,13 +20,16 @@ def isEdge(str1, str2):
             
     return True
 
-def initializeList(filename):
+def initializeList(filename, wordLen):
     # open the file and read all of the words into a list
     words = []
     with open(filename, "r") as file:
         lines = file.readlines()
         for line in lines:
-            words.append(line)
+            line = line.rstrip('\n')
+            # exclude everything that is not the same length as the start/end word
+            if len(line) == wordLen:
+                words.append(line)
     return words
 
 def createMatrix(list):
@@ -51,17 +52,42 @@ def createMatrix(list):
 
 def BFS(list, matrix, start, target):
     # keep track of the parent of every word we gotten. This way we can retrace once we find the target
-    rows, cols = (len(list), len(list))
-    parent = [[0 for i in range(cols)] for j in range(rows)]
+
     # keep track of every word that we check so we dont double check, and get the index of the start word
     checked = []
     index = list.index(start)
+    # a queue of indexs to be checked starting with our start word
+    queue = [index]
     checked.append(index)
+    go = True
+    
     # for each word, if it is connected...
-    for word in matrix[index]:
-        if word == 1:
-            #check if it is the target
-            if list(word) == target:
+    while len(queue) > 0:
+        # use count to keep track of the current potential neighbor index
+        count = 0
+        for potNeighbor in matrix[index]:
+            # if the word differs by one letter...
+            
+            if potNeighbor == 1 and (potNeighbor not in checked):
+                print(count)
+                # it is a neighbor and is not in the checked list, add it to the queue
+                queue.append(count)
+                
+                #check if it is the target
+                if list[count] == target:
+                    return target
+            count += 1
+                    
+            
+        # We checked and added all of index's neighbors so we can add index to checked
+        checked.append(index)
+        # We can remove this from the queue to be checked. This queue structure should insure first in first out BFS
+        queue.pop(0)
+        # Next up
+        if len(queue) == 0:
+            return
+        index = queue[0]
+
 
 
 
@@ -70,15 +96,18 @@ def main():
     filename = args[0]
     start = args[1]
     target = args[2]
+    wordLen = len(start)
 
     # open the file and read all of the words into a list
-    list = initializeList(filename)
+    list = initializeList(filename, wordLen)
     # read through the list and create an adjacency matrix for the words
     matrix = createMatrix(list)
+    #print(matrix)
     # given the list and the adjacency matrix lets make our spanning tree
     # we will make a tuple dictionary 
     # (index1, index2) = distfrom(index -> index2)
-    BFS(list, matrix, start, target)
+    got = BFS(list, matrix, start, target)
+    print(got)
     
 
 if __name__ == '__main__':
